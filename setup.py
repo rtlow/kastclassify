05147748e8258eb48ed40c295d6b572bc048fe98
Ryan Low <rtlow@ucsd.edu>
2020-06-22 19:44:16 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='kastclassify',
+version='0.1',
+description='A Kast Optical Spectrum Analysis Package',
+url='https://github.com/rtlow/kastclassify',
+author='rtlow',
+author_email='rtlow@ku.edu',
+packages=['kastclassify'],
+zip_safe=False)
